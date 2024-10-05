1a3f4018ef52cec7d2f19d12ddfc70cc24536fa9
Reshma <reshmashaik0910@gmail.com>
2024-10-05 14:27:14 +0100

    Added

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement('h1',{},'I am h1 tag')
 console.log(heading);
 
+const jsxHeading = <h1 id='h1'>namaste react</h1>
+console.log(jsxHeading);//here parcel is doing the job behind the scene
+//JSX=>react ele=>js obj=>()browser understandable code(html)
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(jsxHeading);
